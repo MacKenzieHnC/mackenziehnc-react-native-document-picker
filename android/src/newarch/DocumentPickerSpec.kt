b732773828e2333324045d502eea6349ef994e0b
mackenzie <mackenzie@yourmoms>
2023-03-06 12:34:10 -0600

    chore: initial commit

---
+++ android/src/newarch/DocumentPickerSpec.kt
@@ -0,0 +1,7 @@
+package com.documentpicker
+
+import com.facebook.react.bridge.ReactApplicationContext
+
+abstract class DocumentPickerSpec internal constructor(context: ReactApplicationContext) :
+  NativeDocumentPickerSpec(context) {
+}
